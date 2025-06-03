64e2a720ccc352a61e59c7dfbc04eec627eced12
Ahmed-Samerr <ahmed.samer.elkholy@gmail.com>
2025-06-03 22:18:09 +0300

    L.UP

--- src/Components/ProtectedRoute/ProtectedRoute.jsx
+++ src/Components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,7 +1,7 @@
 import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({ children }) {
-  if (localStorage.getItem("userToken")) {
+  if (localStorage.getItem("token")) {
     return children;
   } else {
     return <Navigate to="/login" replace />;
