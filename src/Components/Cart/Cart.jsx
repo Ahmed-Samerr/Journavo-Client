521ce8820f41965f65e775cb0d7c33e11dd5d58f
islam <islammooma95@gmail.com>
2025-06-04 21:09:12 +0300

    ...

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,60 +1,25 @@
-import { useState } from "react";
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom"; // استيراد useNavigate
-import Nile from "../../assets/NileCruise.jpg";
-import NewMuseum from "../../assets/NewMuseum.jpg";
-import sharm from "../../assets/sharm.jpg";
-import BalloonRide from "../../assets/BalloonRide.jpg";
-import Dahab from "../../assets/Dahab.jpg";
+import { UserContext } from "../../Context/UserContext";
+import { removeItemFromCart } from "../../connection/services";
 
 const CartPage = () => {
   const navigate = useNavigate(); // تهيئة useNavigate
-
-  const initialCartItems = [
-    {
-      id: 1,
-      title: "3-Day Nile Cruise - Luxor to Aswan",
-      price: 2500,
-      date: "2025-06-10",
-      image: Nile,
-    },
-    {
-      id: 2,
-      title: "Pyramids & Museum Day Tour - Cairo",
-      price: 800,
-      date: "2025-06-12",
-      image: NewMuseum,
-    },
-    {
-      id: 3,
-      title: "Snorkeling Adventure - Sharm El-Sheikh",
-      price: 1800,
-      date: "2025-07-01",
-      image: sharm,
-    },
-    {
-      id: 4,
-      title: "Hot Air Balloon Ride - Luxor",
-      price: 1200,
-      date: "2025-07-15",
-      image: BalloonRide,
-    },
-    {
-      id: 5,
-      title: "Relaxing Retreat - Dahab",
-      price: 2000,
-      date: "2025-08-05",
-      image: Dahab,
-    },
-  ];
-
-  const [cartItems, setCartItems] = useState(initialCartItems);
+  const { user, setUser, setLoading, setAnimate, isLogin } =
+    useContext(UserContext);
 
   const handleRemove = (id) => {
-    const updatedCart = cartItems.filter((item) => item.id !== id);
-    setCartItems(updatedCart);
+    setLoading(true);
+    setAnimate(true);
+    removeItemFromCart(
+      `/removeItemFromCart/${id}`,
+      setUser,
+      setLoading,
+      setAnimate
+    );
   };
 
-  const total = cartItems.reduce((sum, item) => sum + item.price, 0);
+  const total = 0;
 
   // دالة التوجيه للصفحة
   const handleConfirmBooking = () => {
@@ -79,33 +44,37 @@ const CartPage = () => {
             </tr>
           </thead>
           <tbody className="text-center">
-            {cartItems.map((item) => (
-              <tr
-                key={item.id}
-                className="border-t border-gray-200 hover:bg-blue-50"
-              >
-                <td className="p-4">
-                  <img
-                    src={item.image}
-                    alt={item.title}
-                    className="w-16 h-12 sm:w-20 sm:h-16 object-cover rounded"
-                  />
-                </td>
-                <td className="p-4 font-medium text-gray-800">{item.title}</td>
-                <td className="p-4 text-gray-600">{item.date}</td>
-                <td className="p-4 font-semibold text-blue-700">
-                  {item.price}
-                </td>
-                <td className="p-4">
-                  <button
-                    onClick={() => handleRemove(item.id)}
-                    className="bg-red-100 hover:bg-red-200 text-red-600 px-5 py-2  rounded text-xs sm:text-sm font-medium"
+            {isLogin
+              ? user.cart.map((item) => (
+                  <tr
+                    key={item.id}
+                    className="border-t border-gray-200 hover:bg-blue-50"
                   >
-                    Remove
-                  </button>
-                </td>
-              </tr>
-            ))}
+                    <td className="p-4">
+                      <img
+                        src={item.image}
+                        alt={item.title}
+                        className="w-16 h-12 sm:w-20 sm:h-16 object-cover rounded"
+                      />
+                    </td>
+                    <td className="p-4 font-medium text-gray-800">
+                      {item.title}
+                    </td>
+                    <td className="p-4 text-gray-600">{item.date}</td>
+                    <td className="p-4 font-semibold text-blue-700">
+                      {item.price}
+                    </td>
+                    <td className="p-4">
+                      <button
+                        onClick={() => handleRemove(item._id)}
+                        className="bg-red-100 hover:bg-red-200 text-red-600 px-5 py-2  rounded text-xs sm:text-sm font-medium"
+                      >
+                        Remove
+                      </button>
+                    </td>
+                  </tr>
+                ))
+              : ""}
           </tbody>
         </table>
       </div>
